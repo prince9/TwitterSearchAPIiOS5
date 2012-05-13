95f6b832d25f2cdc85f02e3db852499cb6c80224
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-05-13 17:43:41 +0900

    Initial Commit

---
+++ TweetSearchAPIiOS5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetSearchAPIiOS5
+//
+//  Created by 真有 津坂 on 12/05/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetSearchAPIiOS5/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TweetSearchAPIiOS5
+//
+//  Created by 真有 津坂 on 12/05/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
